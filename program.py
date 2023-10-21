814d6b8e5a69cb2888efc8564f3f1695a2255bb1
Zasada94 <pawelzasada2@wp.pl>
2023-10-21 16:48:39 +0200

    chnges

--- program.py
+++ program.py
@@ -1,3 +1,6 @@
+test
+
+
 def podaj_wiek():
     wiek = None
     while True:
@@ -6,35 +9,45 @@ def podaj_wiek():
             wiek = int(w)
             if wiek >= 0:
                 return wiek
-        print("Nieprawidłowy wiek %s. Musi być liczbą całkowitą większą od zera a jest" % (w,))
+        print(
+            "Nieprawidłowy wiek %s. Musi być liczbą całkowitą większą od zera a jest"
+            % (w,)
+        )
+
 
 def inputGender():
-    plec = ''
-    while plec == '':
-        gender = input('Podaj płeć: (K) - kobieta, (M) - mężczyzna: ')
-        if gender == 'K':
-            plec = 'K'
-        elif gender == 'M':
-            plec = 'M'
+    plec = ""
+    while plec == "":
+        gender = input("Podaj płeć: (K) - kobieta, (M) - mężczyzna: ")
+        if gender == "K":
+            plec = "K"
+        elif gender == "M":
+            plec = "M"
     return plec
-  
+
+
 def wiek(w):
     if w >= 120:
         print("Otrzymujesz darmowy znicz")
     else:
         print("Czym mogę służyć?")
 
+
 def aperol(plec, wiek):
-    if plec == 'K' and wiek >= 30:
+    if plec == "K" and wiek >= 30:
         print("Happy you! Otrzymjesz darmowego aperola")
     else:
         print("Niestety, nie spełniasz kryteriów do otrzymania damarmowego drinka")
 
+
 def malboro(plec, wiek, region):
-    if plec == 'M' and wiek >= 40 and region == 'U':
+    if plec == "M" and wiek >= 40 and region == "U":
         print("Gratulacje! Otrzymujesz jedną paczkę papierosów Malboro gratis!")
     else:
-        print("Niestety, nie spełniasz kryteriów do otrzymania darmowej paczki papierosów Malboro :(")
+        print(
+            "Niestety, nie spełniasz kryteriów do otrzymania darmowej paczki papierosów Malboro :("
+        )
+
 
 def wybierz_region():
     while True:
