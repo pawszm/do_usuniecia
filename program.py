8acdd166fe4bdb6f6ffc8d59981f4106de97f16d
klaudiapacewicz <klaudia.pacewicz1@gmail.com>
2023-10-21 11:37:02 +0000

    poprawka kodu

--- program.py
+++ program.py
@@ -1,30 +1,25 @@
 
 
-
-import random
-
 wybor_plci = ['K', 'M']
-plec = input("Podaj swoja plec:\n"
-             "a) K\n"
-             "b) M\n")
-
-for index, item in enumerate(wybor_plci):
-    print(plec)
-
+plec = input("Podaj swoja plec: ")
+wiek = input("Podaj swoj wiek: ")
+region = input("Podaj region: ")
+wiek = int(wiek)
 
 def aperol():
-    if plec = K && wiek = 30:
+    if plec == 'K' and wiek == 30:
         print("Happy you! Otrzymjesz darmowego aperola")
     else:
         print("Niestety, nie spełniasz kryteriów do otrzymania damarmowego drinka")
 
 def malboro():
-    if plec = M && wiek >= 40 && region = U:
+    if plec == 'M' and wiek >= 40 and region == 'U':
         print("Gratulacje! Otrzymujesz jedną paczkę papierosów Malboro gratis!")
     else:
         print("Niestety, nie spełniasz kryteriów do otrzymania darmowej paczki papierosów Malboro :(")
 
-
+print(aperol())
+print(malboro())
 
 
 
