43cb7bb303f59991110977e7943c9a69eaaaa852
Student Podyplomowy <mpwegierek@gmail.com>
2023-10-21 13:46:22 +0200

    Marek v1

--- program.py
+++ program.py
@@ -1,5 +1,17 @@
-w = int( input("Podaj wiek:") )
-if w > 17:
-    print("Stary cap")
-else:
-    print("Niepełnioletni")
+# w = int( input("Podaj wiek:") )
+# if w > 17:
+#     print("Stary cap")
+# else:
+#     print("Niepełnioletni")
+#
+def inputGender():
+    plec = ''
+    while plec == '':
+        gender = input('Podaj płeć: (K) - kobieta, (M) - mężczyzna: ')
+        if gender == 'K':
+            plec = 'K'
+        elif gender == 'M':
+            plec = 'M'
+    return plec
+plec = inputGender()
+print(plec)
