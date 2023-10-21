4cd5c502b7e5686c02b6264685a2a893218d4763
klaudiapacewicz <klaudia.pacewicz1@gmail.com>
2023-10-21 11:08:14 +0000

    funkcje Klaudii

--- program.py
+++ program.py
@@ -1,5 +1,37 @@
-w = int( input("Podaj wiek:") )
-if w > 17:
-    print("Stary cap")
-else:
-    print("Niepełnioletni")
+
+
+
+import random
+
+wybor_plci = ['K', 'M']
+plec = input("Podaj swoja plec:\n"
+             "a) K\n"
+             "b) M\n")
+
+for index, item in enumerate(wybor_plci):
+    print(plec)
+
+
+def aperol():
+    if plec = K && wiek = 30:
+        print("Happy you! Otrzymjesz darmowego aperola")
+    else:
+        print("Niestety, nie spełniasz kryteriów do otrzymania damarmowego drinka")
+
+def malboro():
+    if plec = M && wiek >= 40 && region = U:
+        print("Gratulacje! Otrzymujesz jedną paczkę papierosów Malboro gratis!")
+    else:
+        print("Niestety, nie spełniasz kryteriów do otrzymania darmowej paczki papierosów Malboro :(")
+
+
+
+
+
+
+
+
+
+
+
+
