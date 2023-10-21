30f7e8e853a42963204ea0da0ae5e672e33dcef3
dominikjaruga <dominik.jaruga@gmail.com>
2023-10-21 15:17:16 +0200

    fragment Dominik_funkcja

--- program.py
+++ program.py
@@ -1,8 +1,10 @@
-
 w = input("Podaj wiek: ")
 w=int(w)
 
-if w >= 120:
-    print("Takich dziadów nie obsługujemy")
-else:
-    print("Czym mogę służyć?")
+def wiek(w):
+    if w >= 120:
+        print("Takich dziadów nie obsługujemy")
+    else:
+        print("Czym mogę służyć?")
+print(wiek(w))
+
