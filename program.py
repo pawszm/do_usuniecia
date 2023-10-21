3ace6bcd271f93a797adaa15ff0bba7cc4d86553
pawszm <pawszm71@gmail.com>
2023-10-21 14:17:50 +0200

    Age update function by Pawel

--- program.py
+++ program.py
@@ -1,5 +1,17 @@
-w = int( input("Podaj wiek:") )
-if w > 17:
-    print("Stary cap")
-else:
-    print("Niepełnioletni")
+def podaj_wiek():
+    wiek = None
+    while True:
+        w = input("Podaj wiek: ")
+        if w.isdigit():
+            wiek = int(w)
+            if wiek >= 0:
+                return wiek
+        print("Nieprawidłowy wiek %s. Musi być liczbą całkowitą większą od zera a jest" % (w,))
+
+# if w > 17:
+#     print("Stary cap")
+# else:
+#     print("Niepełnioletni")
+
+wiek = podaj_wiek()
+print(wiek)
